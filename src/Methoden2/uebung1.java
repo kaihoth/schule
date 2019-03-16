5eb1103f902d1d575c1ac7ead50ed07cfb39eb7c
Kai Hoth <hoth.kai@yahoo.com>
2019-03-16 14:23:58 +0100

    added comment what this Software does

--- src/Methoden2/uebung1.java
+++ src/Methoden2/uebung1.java
@@ -2,6 +2,30 @@ package Methoden2;
 import java.util.*;
 
 public class uebung1 {
+    /*
+        Dises Programm generiert Matrizen und verabreitet diese in verschiedenen Methoden
+
+
+
+        Schreiben Sie ein Programm zur Verwaltung von Matrizen für natürliche Zahlen. Die Matrizen sollen in Arrays gehalten werden. Das Programm soll über folgende Methoden verfügen:
+
+        Methode eingabe():
+        Für die Matrize kann der Anwender die Dimension (Anzahl der Zeilen und Spalten) der Matrize, sowie deren Werte eingeben. Die Methode hat keine Eingabeparameter und gibt keinen Wert zurück.
+
+        Methode ausgabe():
+        Die übergebene Matrize wird formatiert auf dem Bildschirm ausgegeben.
+
+        Methode transponieren():
+        Die übergebene Matrize wird transponiert. Das bedeutet, dass in der zu transponierenden Matrize die Werte wie folgt getauscht werden: matrix[i][j] = matrix [j][i].
+
+        Methode addieren():
+        Zwei übergebene Matrizen (deren Anzahl an Zeilen und Spalten identisch sind) werden addiert in dem jedes Element der einen Matrize zu dem Element mit denselben Indizes der anderen Matrize addiert wird. Das Ergebnis der Addition wird in einer anderen Matrize als die übergebenen Matrizen gespeichert.
+
+        Methode multiplizieren():
+        Zwei übergebene Matrizen (deren Anzahl an Zeilen und Spalten identisch sind) werden nach folgendem Schema multipliziert: Die erste Zeile der ersten Matrize wird mit der ersten Spalte der zweiten Matrize elementweise multipliziert. Anschließend werden die Produkte addiert. Dieses Ergebnis bildet das erste Element der Ergebnis-Matrize.
+
+
+     */
     public static int[][] matrize1, matrize2;
 
     public static void main(String[] args) {
